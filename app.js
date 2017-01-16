18b366f6a36bf429ff7e98c2e20958db61374e78
cleverett1@gmail.com <cleverett1@gmail.com>
2017-01-16 18:17:20 -0500

    simple routes

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ app.listen(3000, function(){
 // app.listen sets up the server, from this point on everything is hooked up.
 
 app.get('/', function(req, res, next){
-  res.send("hooray");
+  res.send("welcome");
+})
+
+// another route for when client is trying to reach localhost:3000/news
+app.get('/news', function(req, res, next){
+  res.send("hooray for news");
 })
