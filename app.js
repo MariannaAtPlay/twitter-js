ab63961ffd8b8f170ab884799033d7466e9bc74c
cleverett1@gmail.com <cleverett1@gmail.com>
2017-01-17 11:14:14 -0500

    basic template

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 
+// app = require('express')();
+
+const volleyball = require('volleyball'); 
+const nunjucks = require('nunjucks');
+
 app.listen(3000, function(){
   console.log('server listening');
 });
 
 // app.listen sets up the server, from this point on everything is hooked up.
-
 app.use(function (req, res, next) {
     // do your logging here
-    console.log('Request Type:', req.method, 'Request URL:', req.originalUrl);
+    console.log('Request Type:', req.method, req.originalUrl);
+    console.log('Status Code:', res.statusCode);
+    // console.log('Path:',  req.path); <--- does similiar/if not same thing.
     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
     next();
 })
+
+app.use(volleyball);
 
 app.get('/', function(req, res, next){
   res.send("welcome");
