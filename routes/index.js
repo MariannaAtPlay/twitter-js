cce141621e7d257df1b435dc66af2dae07429a57
Marianna Shvartsapel <marianna.shvartsapel@gmail.com>
2017-01-17 15:52:11 -0500

    style.css request is working

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
@@ -8,9 +9,17 @@ router.get('/', function (req, res) {
   res.render( 'index', { tweets: tweets } );
 });
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+router.get('/index.html', function (req, res) {
+  //let tweets = tweetBank.list();
+  res.send('hello');
 });
+
+router.get('/stylesheets/style.css', function (req, res) {
+  //let tweets = tweetBank.list();
+  //res.sendFile('/Users/Marianna/Documents/Grace Hopper/Junior/twitter-js/public/stylesheets/style.css');
+  res.sendFile('style.css', { root: path.join(__dirname, '../public/stylesheets/') });
+});
+
+
 
 module.exports = router;
